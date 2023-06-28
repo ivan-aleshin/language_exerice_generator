c01b456fddc55fa5bafd40fa9fc142a4ccfa3f03
Ivan Aleshin <ivan.aleshin.ai@gmail.com>
2023-06-28 17:58:59 +0400

    added template decorator added return for exercises methods

--- exercise_gen.py
+++ exercise_gen.py
@@ -9,26 +9,32 @@ from lang_proj.tokens import HUGGINGFACE_API_TOKEN
 
 
 class ExerciseGenerator:
-    nlp = spacy.load('en_core_web_sm')
-    tags_ = ['ADJ', 'DET', 'NOUN', 'VERB']
+    _nlp = spacy.load('en_core_web_sm')
+    _tags = ['ADJ', 'DET', 'NOUN', 'VERB']
 
     def __init__(self, filename: str, encoding: str = 'utf-8'):
         self.word_vectors = api.load("glove-wiki-gigaword-100")
         self.text_data = ExerciseGenerator.text_to_dataset(filename)
-        #self.output
 
-    def generate(self, max_num: int = 10):
-        pass
-
-    def permutation(self, max_num: int = 1):
-        pass
+    def _template_wrapper(exercise_generator):
+        def wrapper(*args, **kwargs):
+            result = exercise_generator(*args, **kwargs)
+            task = {'type': result[0],
+                    'description': result[1],
+                    'sentence': result[2],
+                    'options': result[3],
+                    'answer': result[4],
+                    'result': '',
+                    'total': 0}
+            return task
+        return wrapper
 
     @staticmethod
     def split_blocks(block: str):
         """
         Returns splitted block into separate sentences
         """
-        doc = ExerciseGenerator.nlp(block)
+        doc = ExerciseGenerator._nlp(block)
 
         return [sent.text.strip() for sent in doc.sents]
 
@@ -58,7 +64,7 @@ class ExerciseGenerator:
 
     @staticmethod
     def add_part_of_word(sentence):
-        doc = ExerciseGenerator.nlp(sentence)
+        doc = ExerciseGenerator._nlp(sentence)
         part_of_word = []
         for pow in doc:
             if pow.pos_ not in part_of_word:
@@ -67,7 +73,7 @@ class ExerciseGenerator:
 
     def choose_sentence(self, option=None, limits=(3, 15)):
         if option is None:
-            option = random.choice(ExerciseGenerator.tags_)
+            option = random.choice(ExerciseGenerator._tags)
         index_tag = self.text_data[(self.text_data['part_of_word'].str.contains(option))].index
         index_limit = self.text_data[(self.text_data['line_length'] >= limits[0]) &
                                      (self.text_data['line_length'] <= limits[1])].index
@@ -78,16 +84,17 @@ class ExerciseGenerator:
         else:
             return 'The Stogovs left their flat early in the morning and went by bus to the country.'
 
+    @_template_wrapper
     def gen_missings(self, tag=None, options=True):
         if tag is None:
-            tag = random.choice(ExerciseGenerator.tags_)
+            tag = random.choice(ExerciseGenerator._tags)
         sentence = self.choose_sentence(tag, limits=(5, 18))
         if not options:
             try:
                 sentence_trans = ExerciseGenerator.translate(sentence)
             except:
                 options = True
-        target_word = random.choice([str(token) for token in ExerciseGenerator.nlp(sentence) if token.pos_ == tag])
+        target_word = random.choice([str(token) for token in ExerciseGenerator._nlp(sentence) if token.pos_ == tag])
         blank_line = ' ____ '
         question_sentence = re.sub(fr'\b{target_word}\b', blank_line, sentence, count=1, flags=re.I)
         if options:
@@ -95,16 +102,25 @@ class ExerciseGenerator:
             wrong_answers = map(lambda x: x[0], self.word_vectors.most_similar(target_word.lower())[:3])
             answers.extend(wrong_answers)
             random.shuffle(answers)
-            print('Fill the gap in the sentence:')
-            print(question_sentence)
-            print('Options:', answers)
-            print('Right answer:', target_word)
+            return ('missings_with_options',
+                    ['Choose the missing word in the sentence:',
+                    'Выберите пропущенное слово в предложении:'],
+                    question_sentence,
+                    answers,
+                    target_word.lower(),
+                    '',
+                    0)
         else:
-            print('Fill the missing word')
-            print(question_sentence)
-            print('Translation:', sentence_trans)
-            print('Right answer:', target_word)
+            return ('missings_no_options',
+                    ['Write down the missing word in the sentence (only one in lowercase):',
+                    'Впишите пропущенное слово в предложении (одно, в нижнем регистре):'],
+                    question_sentence,
+                    sentence_trans,
+                    target_word.lower(),
+                    '',
+                    0)
 
+    @_template_wrapper
     def gen_shuffle(self, translation=True):
         if translation:
             sentence = self.choose_sentence(limits=(7, 12))
@@ -113,16 +129,21 @@ class ExerciseGenerator:
             except:
                 gen_shuffle(self, translation=False)
             right_ans, shuffled = ExerciseGenerator.shuffle(sentence)
-            print('Choose the right order of words')
-            print(sentence_trans)
-            print('Words:', shuffled)
-            print('Right answer:', right_ans)
+            return ('shuffle_with_translation',
+                   ['Choose the right order of words',
+                   'Выберите правильный порядок слов в предложении'],
+                    sentence_trans,
+                    shuffled,
+                    right_ans)
         else:
             sentence = self.choose_sentence(limits=(3, 6))
             right_ans, shuffled = ExerciseGenerator.shuffle(sentence)
-            print('Choose the right order of words')
-            print('Words:', shuffled)
-            print('Right answer:', right_ans)
+            return ('shuffle_no_translation',
+                   ['Choose the right order of words',
+                   'Выберите правильный порядок слов в предложении'],
+                   '',
+                   shuffled,
+                   right_ans)
 
     @staticmethod
     def shuffle(sentence):
@@ -139,11 +160,20 @@ class ExerciseGenerator:
                                  from_language='en',
                                  to_language='ru')
 
+    @_template_wrapper
     def gen_question(self):
         sentence = self.choose_sentence(option='NOUN', limits=(7, 12))
-        target_word = random.choice([str(token) for token in ExerciseGenerator.nlp(sentence) if token.pos_ == 'NOUN'])
+        target_word = random.choice([str(token) for token in ExerciseGenerator._nlp(sentence) if token.pos_ == 'NOUN'])
         question = ExerciseGenerator.huggin_question(sentence, target_word)
-        print(sentence, question, 'Type only one word in lower case', target_word.lower(), sep='\n')
+        return ('question',
+                [
+                    'Right down the answer for this question (only one word in lower case):',
+                    'Напишите ответ на вопрос (одно слово по английски в нижнем регистре)'
+                ],
+                sentence,
+                question,
+                target_word.lower()
+               )
 
 
     def long_read(self):
@@ -157,3 +187,8 @@ class ExerciseGenerator:
         text = f'answer: {answer} context: {sentence}'
         response = requests.post(API_URL, headers=headers, json={"inputs": text,})
         return response.json()[0]['generated_text']
+
+    def output(self, n_exercises: int=10, types=None):
+        output = {}
+        for i in range(n_exercises):
+            pass
